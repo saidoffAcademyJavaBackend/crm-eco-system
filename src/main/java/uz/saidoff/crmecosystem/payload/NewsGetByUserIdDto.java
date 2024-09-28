992d79a869205341242ee083573292ab600c6f11
Umrbek004 <kuchkarovumrbek9@gmail.com>
2024-09-28 22:13:53 +0500

    news bugs fixed

--- src/main/java/uz/saidoff/crmecosystem/payload/NewsGetByUserIdDto.java
+++ src/main/java/uz/saidoff/crmecosystem/payload/NewsGetByUserIdDto.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import uz.saidoff.crmecosystem.enums.RoleType;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -17,7 +19,7 @@ public class NewsGetByUserIdDto {
     private String title;
     private String content;
     private UUID attachmentId;
-    private String roleType;
+    private List<RoleType> roleType;
     private UUID createdBy;
     private Date createdDate;
 }
