ed51b7141e77dc9347d6979e60247733e1d08eda
@Nekmurodov <nekmurodovdilshodbek@gmail.com>
2024-10-11 17:57:01 +0500

    Task comment

--- src/main/java/uz/saidoff/crmecosystem/repository/SpecialityRepository.java
+++ src/main/java/uz/saidoff/crmecosystem/repository/SpecialityRepository.java
@@ -3,6 +3,7 @@ package uz.saidoff.crmecosystem.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uz.saidoff.crmecosystem.entity.Speciality;
 
 import java.util.Optional;
@@ -12,5 +13,6 @@ public interface SpecialityRepository extends JpaRepository<Speciality, UUID> {
     Optional<Speciality> findByName(String name);
     Optional<Speciality> findByIdAndDeletedFalse(UUID id);
     Boolean existsByNameAndDeletedFalse(String name);
+    @Query("SELECT s FROM Speciality s WHERE s.deleted = false")
     Page<Speciality> findAllDeletedFalse(Pageable pageable);
 }
--- src/main/java/uz/saidoff/crmecosystem/repository/TaskCommentRepository.java
+++ src/main/java/uz/saidoff/crmecosystem/repository/TaskCommentRepository.java
@@ -3,6 +3,7 @@ package uz.saidoff.crmecosystem.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uz.saidoff.crmecosystem.entity.TaskComment;
 
 import java.util.Optional;
@@ -10,5 +11,6 @@ import java.util.UUID;
 
 public interface TaskCommentRepository extends JpaRepository<TaskComment, UUID> {
     Optional<TaskComment> findByTaskIdAndDeletedFalse(UUID taskId);
+    @Query("SELECT s FROM TaskComment s WHERE s.deleted = false")
     Page<TaskComment> findAllDeletedFalse(Pageable pageable);
 }
