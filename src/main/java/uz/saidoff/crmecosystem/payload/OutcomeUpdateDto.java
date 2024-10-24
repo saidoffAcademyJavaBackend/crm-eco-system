9f8a4e62df3696c351b449d7cf37b22b3913aebb
@Nekmurodov <nekmurodovdilshodbek@gmail.com>
2024-10-24 15:39:07 +0500

    outcome

---
+++ src/main/java/uz/saidoff/crmecosystem/payload/OutcomeUpdateDto.java
@@ -0,0 +1,21 @@
+package uz.saidoff.crmecosystem.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.saidoff.crmecosystem.enums.Currency;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OutcomeUpdateDto {
+    private UUID outcomeId;
+    private Double amount;
+    private String description;
+    private Currency currency;
+    private UUID categoryId;
+    private Boolean isIncome;
+    private UUID attachmentId;
+}
