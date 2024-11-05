87b1ddd0cf6547ffd9af16f58a3dd8165066b3c9
@Nekmurodov <nekmurodovdilshodbek@gmail.com>
2024-11-05 20:53:14 +0500

    employee edit

---
+++ src/main/java/uz/saidoff/crmecosystem/payload/EmployeeWarningDto.java
@@ -0,0 +1,14 @@
+package uz.saidoff.crmecosystem.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeWarningDto {
+    boolean warning;
+}
