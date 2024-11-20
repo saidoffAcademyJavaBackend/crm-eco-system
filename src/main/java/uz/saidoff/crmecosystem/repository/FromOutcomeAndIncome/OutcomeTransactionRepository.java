e0289a953f6cd401b1a0f5f245f32de613a2aed4
@Nekmurodov <nekmurodovdilshodbek@gmail.com>
2024-11-20 19:00:39 +0500

    update

--- src/main/java/uz/saidoff/crmecosystem/repository/FromOutcomeAndIncome/OutcomeTransactionRepository.java
+++ src/main/java/uz/saidoff/crmecosystem/repository/FromOutcomeAndIncome/OutcomeTransactionRepository.java
@@ -3,24 +3,22 @@ package uz.saidoff.crmecosystem.repository.FromOutcomeAndIncome;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uz.saidoff.crmecosystem.enums.Currency;
 import uz.saidoff.crmecosystem.payload.OutcomeAndIncome.OutcomeHistory;
+import uz.saidoff.crmecosystem.payload.OutcomeAndIncome.OutcomeHistoryList;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
-public class OutcomeTransaction {
+public class OutcomeTransactionRepository {
 
-    @Autowired
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
-    public OutcomeHistory getOutcomeBreakdownBetweenDates(Date startDate, Date endDate, Currency currency,
-                                                          String employee, String advertisement, String another) {
+    public OutcomeHistoryList getOutcomeBreakdownBetweenDates(Date startDate, Date endDate, Currency currency) {
         String sql = "SELECT c.name, SUM(t.amount) " +
                 "FROM Transaction t " +
                 "JOIN t.category c " +
@@ -37,28 +35,27 @@ public class OutcomeTransaction {
 
         double totalOutcome = results.stream().mapToDouble(row -> (Double) row[1]).sum();
 
-        OutcomeHistory outcomeHistory = new OutcomeHistory();
-        outcomeHistory.setAllOutcomesSum(totalOutcome);
+        return getOutcomeHistoryList(totalOutcome, results);
+    }
+
+    private static OutcomeHistoryList getOutcomeHistoryList(double totalOutcome, List<Object[]> results) {
+        OutcomeHistoryList outcomeHistoryList = new OutcomeHistoryList();
+        outcomeHistoryList.setAllAmount(totalOutcome);
+
+        List<OutcomeHistory> list = new ArrayList<>();
 
         for (Object[] row : results) {
             String categoryName = (String) row[0];
             double amount = (Double) row[1];
             double percentage = (amount / totalOutcome) * 100;
 
-            if (Objects.equals(categoryName, employee)) {
-                outcomeHistory.setFromEmployee(amount);
-                outcomeHistory.setInterestOnEmployee(percentage);
-            }
-            else if (Objects.equals(categoryName, advertisement)) {
-                outcomeHistory.setFromAdvertisement(amount);
-                outcomeHistory.setInterestOnAdvertisement(percentage);
-            }
-            else if (Objects.equals(categoryName, another)) {
-                outcomeHistory.setFromAnother(amount);
-                outcomeHistory.setInterestOnAnother(percentage);
-            }
+            OutcomeHistory outcomeHistory = new OutcomeHistory();
+            outcomeHistory.setNameCategory(categoryName);
+            outcomeHistory.setAmount(amount);
+            outcomeHistory.setCategoryInterest(percentage);
+            list.add(outcomeHistory);
         }
-
-        return outcomeHistory;
+        outcomeHistoryList.setOutcomeHistory(list);
+        return outcomeHistoryList;
     }
 }
