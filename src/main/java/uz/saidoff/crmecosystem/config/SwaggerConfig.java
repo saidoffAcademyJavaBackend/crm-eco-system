6ec6640f0b2281d1203b959658e5616a378eea37
new token <ibroximjon.yursunov@fido.uz>
2024-12-18 19:27:42 +0500

    make changes in cors configuration

--- src/main/java/uz/saidoff/crmecosystem/config/SwaggerConfig.java
+++ src/main/java/uz/saidoff/crmecosystem/config/SwaggerConfig.java
@@ -6,25 +6,34 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @OpenAPIDefinition(info = @Info(
-        description = "OpenAPI documentation for CRM EcoSystem",
-        title = "CRM EcoSystem",
-        version = "1.0"
+  description = "OpenAPI documentation for CRM EcoSystem",
+  title = "CRM EcoSystem",
+  version = "1.0"
 ),
-        security = @SecurityRequirement(
-                name = "BearerAuth"
-        )
+  security = @SecurityRequirement(
+    name = "BearerAuth"
+  )
 )
 @SecurityScheme(
-        name = "BearerAuth",
-        description = "JWT-based authentication",
-        scheme = "bearer",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        in = SecuritySchemeIn.HEADER
+  name = "BearerAuth",
+  description = "JWT-based authentication",
+  scheme = "bearer",
+  type = SecuritySchemeType.HTTP,
+  bearerFormat = "JWT",
+  in = SecuritySchemeIn.HEADER
 )
 @Configuration
 public class SwaggerConfig {
+        @Bean
+        public OpenApiCustomizer openApiCustomizer() {
+                return openApi -> openApi.servers(List.of(new Server().url("https://back.crm.saidoff.uz")));
+        }
 }
--- src/main/java/uz/saidoff/crmecosystem/config/security/SecurityConfig.java
+++ src/main/java/uz/saidoff/crmecosystem/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,47 +18,48 @@ import uz.saidoff.crmecosystem.security.JWTFilter;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig  {
+public class SecurityConfig {
 
-    private final AuthenticationProvider authenticationProvider;
-    private final JWTFilter jwtFilter;
-    private final LogoutHandler logoutHandler;
-    private final CustomAccessDeniedHandler accessDeniedHandler;
-    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+  private final AuthenticationProvider authenticationProvider;
+  private final JWTFilter jwtFilter;
+  private final LogoutHandler logoutHandler;
+  private final CustomAccessDeniedHandler accessDeniedHandler;
+  private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-       http
-               .csrf(AbstractHttpConfigurer::disable)
-               .authorizeHttpRequests(
-                       auth -> auth
-                               .requestMatchers(
-                                       "api/v1/auth/**",
-                                       "/v2/api-docs",
-                                       "/v3/api-docs",
-                                       "/v3/api-docs/**",
-                                       "/swagger-resources",
-                                       "/swagger-resources/**",
-                                       "/configuration/ui",
-                                       "/configuration/security",
-                                       "/swagger-ui/**",
-                                       "/webjars/**",
-                                       "/swagger-ui.html/**").permitAll()
-                               .anyRequest().authenticated()
-               )
-               .authenticationProvider(authenticationProvider)
-               .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-               .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-               .logout(logout -> logout
-                       .logoutUrl("/logout")
-                       .addLogoutHandler(logoutHandler)
-                       .logoutSuccessHandler((_, _, _) -> SecurityContextHolder.clearContext())
-               )
-               .exceptionHandling(exception -> exception
-                       .accessDeniedHandler(accessDeniedHandler)
-                       .authenticationEntryPoint(authenticationEntryPoint)
-               );
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+      .csrf(AbstractHttpConfigurer::disable)
+      .cors(Customizer.withDefaults())
+      .authorizeHttpRequests(
+        auth -> auth
+          .requestMatchers(
+            "api/v1/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html/**").permitAll()
+          .anyRequest().authenticated()
+      )
+      .authenticationProvider(authenticationProvider)
+      .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .logout(logout -> logout
+        .logoutUrl("/logout")
+        .addLogoutHandler(logoutHandler)
+        .logoutSuccessHandler((_, _, _) -> SecurityContextHolder.clearContext())
+      )
+      .exceptionHandling(exception -> exception
+        .accessDeniedHandler(accessDeniedHandler)
+        .authenticationEntryPoint(authenticationEntryPoint)
+      );
 
-       return http.build();
-    }
+    return http.build();
+  }
 }
